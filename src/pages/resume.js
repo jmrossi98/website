15ae947290aa318155d1a56b8a146b9c916ffd68
jake.rossi <jake@mastodondesign.com>
2021-10-12 13:11:01 -0400

    Better spacing, got rid of unused imports

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf'
-import styled from 'styled-components';
 import file from '../assets/resume.pdf';
-
-const DocumentWrapper = styled("div")({
-    maxHeight: "600px",
-    overflowY: "auto"
-  });
 
 const Resume = () => {
     return (
         <iframe
-            style={{ width: "1420px", height: "1000px" }}
+            style={{ width: "1436px", height: "642px" }}
             src={file}
             type='application/pdf'
             title='title'
