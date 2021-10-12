8ef548fa78c6a6380de61ffba792392ffff474ce
jmrossi98 <jmrossi98@gmail.com>
2021-10-12 01:29:36 -0400

    Added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,129 @@
+import { FaBars } from 'react-icons/fa';
+import { NavLink as Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLogo to='/'>
+          Jakob Rossi
+        </NavLogo>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/' activeStyle>
+            Projects
+          </NavLink>
+          <NavLink to='/' activeStyle>
+            Resume
+          </NavLink>
+          <NavLink to='/' activeStyle>
+            Contact
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+
+export const Nav = styled.nav`
+  background: #a5dcf4;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.2rem calc((55vw - 1000px) / 2);
+  z-index: 12;
+  /* Third Nav */
+  /* justify-content: flex-start; */
+`;
+  
+export const NavLink = styled(Link)`
+  color: #808080;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  &.active {
+    color: #000000;
+  }
+`;
+
+export const NavLogo = styled(Link)`
+  color: #808080;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  &.active {
+    color: #000000;
+  }
+  font-size: 250%;
+`;
+
+export const Bars = styled(FaBars)`
+  display: none;
+  color: #808080;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-90%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+  
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -24px;
+  /* Second Nav */
+  /* margin-right: 24px; */
+  /* Third Nav */
+  /* width: 100vw;
+  white-space: nowrap; */
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+  
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: right;
+  margin-right: 50px;
+  /* Third Nav */
+  /* justify-content: flex-end;
+  width: 100vw; */
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+  
+export const NavBtnLink = styled(Link)`
+  border-radius: 4px;
+  background: #808080;
+  padding: 5px 15px;
+  color: #000000;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  /* Second Nav */
+  margin-left: 50px;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #808080;
+  }
+`;
+
+export default Navbar;
---
+++ src/pages/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+const Home = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Left',
+        alignItems: 'Left',
+        height: '90vh',
+      }}
+    >
+
+    </div>
+  );
+};
+  
+export default Home;
