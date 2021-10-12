3f19a925cfcf79c670b186d5a110dcf590d3ca15
jake.rossi <jake@mastodondesign.com>
2021-10-12 13:18:16 -0400

    Resume view adjusts to different screen sizes

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,7 +4,7 @@ import file from '../assets/resume.pdf';
 const Resume = () => {
     return (
         <iframe
-            style={{ width: "1436px", height: "642px" }}
+            style={{ position:"fixed", top:"100", left:"0", bottom:"100", right:"0", width:"100%", height:"100%", border:"none", margin:"0", padding:"0"}}
             src={file}
             type='application/pdf'
             title='title'
